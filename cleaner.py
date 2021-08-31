cfb1c2c0cb98a556a5bb1dc4723d43983485f2d4
frankysoo <your.email@example.com>
2021-09-01 00:00:00 +0100

    Implement data quality scoring

--- cleaner.py
+++ cleaner.py
@@ -1025,3 +1025,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:32:33
 
 # Improved cleaning algorithm: 2025-04-17 20:32:34
+
+# Improved cleaning algorithm: 2025-04-17 20:57:07
