4badd16a693a3d7a7b2cf575bf2f1cfcc5429e53
frankysoo <your.email@example.com>
2021-09-05 12:08:15 +0100

    Optimize CSV loading for large files

--- data_loader.py
+++ data_loader.py
@@ -794,3 +794,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:32:37
 
 # Enhanced data loading: 2025-04-17 20:57:07
+
+# Enhanced data loading: 2025-04-17 20:57:08
