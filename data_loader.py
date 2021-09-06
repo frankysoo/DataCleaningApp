ddd0c1f706e6c4d1d2c05580ce822bf4e11f1921
frankysoo <your.email@example.com>
2021-09-06 05:50:16 +0100

    Add multi-file upload support

--- data_loader.py
+++ data_loader.py
@@ -796,3 +796,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:07
 
 # Enhanced data loading: 2025-04-17 20:57:08
+
+# Enhanced data loading: 2025-04-17 20:57:08
