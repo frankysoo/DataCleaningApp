50ed5c36aeeac331dd27dd9df8209b429f11166e
frankysoo <your.email@example.com>
2021-09-11 19:50:10 +0100

    Improve error logging for debugging

--- data_loader.py
+++ data_loader.py
@@ -800,3 +800,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:08
 
 # Enhanced data loading: 2025-04-17 20:57:09
+
+# Enhanced data loading: 2025-04-17 20:57:09
