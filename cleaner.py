462e229746e6e9af38d304a8c2da85ab070de2b5
frankysoo <your.email@example.com>
2021-10-05 00:37:27 +0100

    Add usage examples to documentation

--- cleaner.py
+++ cleaner.py
@@ -1031,3 +1031,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:08
 
 # Improved cleaning algorithm: 2025-04-17 20:57:11
+
+# Improved cleaning algorithm: 2025-04-17 20:57:13
