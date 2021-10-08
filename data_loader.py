c3f95281449ad34f4d23ba6474bb0fe84094b763
frankysoo <your.email@example.com>
2021-10-08 22:33:52 +0100

    Improve file encoding detection

--- data_loader.py
+++ data_loader.py
@@ -806,3 +806,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:10
 
 # Enhanced data loading: 2025-04-17 20:57:11
+
+# Enhanced data loading: 2025-04-17 20:57:14
