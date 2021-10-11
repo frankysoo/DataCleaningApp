6e3859b0c45ee3277e3eaf3bc63c447a6d75a959
frankysoo <your.email@example.com>
2021-10-11 10:34:31 +0100

    Fix null handling in numeric columns

--- cleaner.py
+++ cleaner.py
@@ -1039,3 +1039,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:13
 
 # Improved cleaning algorithm: 2025-04-17 20:57:14
+
+# Improved cleaning algorithm: 2025-04-17 20:57:14
