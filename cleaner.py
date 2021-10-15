fbba0eabc05c77b509500780fe803729f1041a8d
frankysoo <your.email@example.com>
2021-10-15 23:50:01 +0100

    Add multi-file upload support

--- cleaner.py
+++ cleaner.py
@@ -1043,3 +1043,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:14
 
 # Improved cleaning algorithm: 2025-04-17 20:57:15
+
+# Improved cleaning algorithm: 2025-04-17 20:57:15
