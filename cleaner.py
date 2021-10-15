675ec0f190ca2c66f88b7e470107665d74f988c1
frankysoo <your.email@example.com>
2021-10-15 16:59:55 +0100

    Optimize memory usage for very large files

--- cleaner.py
+++ cleaner.py
@@ -1041,3 +1041,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:14
 
 # Improved cleaning algorithm: 2025-04-17 20:57:14
+
+# Improved cleaning algorithm: 2025-04-17 20:57:15
