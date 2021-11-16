425b0bd62807ebb555ad76d7c24a4e8416359f26
frankysoo <your.email@example.com>
2021-11-16 11:40:21 +0100

    Improve error recovery for large file processing

--- data_loader.py
+++ data_loader.py
@@ -820,3 +820,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:19
 
 # Enhanced data loading: 2025-04-17 20:57:20
+
+# Enhanced data loading: 2025-04-17 20:57:22
