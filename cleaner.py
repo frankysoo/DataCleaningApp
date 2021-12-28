24da4ff88d09c099f188687b4c9dc24d20e39b56
frankysoo <your.email@example.com>
2021-12-28 11:40:16 +0100

    Add advanced filtering options

--- cleaner.py
+++ cleaner.py
@@ -1067,3 +1067,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:25
 
 # Improved cleaning algorithm: 2025-04-17 20:57:25
+
+# Improved cleaning algorithm: 2025-04-17 20:57:29
