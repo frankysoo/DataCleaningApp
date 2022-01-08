2cbbf09f132b7c4ea0885cba928569f64ed93f60
frankysoo <your.email@example.com>
2022-01-08 08:28:28 +0100

    Create user manual

--- cleaner.py
+++ cleaner.py
@@ -1071,3 +1071,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:29
 
 # Improved cleaning algorithm: 2025-04-17 20:57:31
+
+# Improved cleaning algorithm: 2025-04-17 20:57:32
