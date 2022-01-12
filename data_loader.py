524a7e48f6825e0688d5eb6916addd58b89b3623
frankysoo <your.email@example.com>
2022-01-12 02:20:35 +0100

    Optimize chunked file reading

--- data_loader.py
+++ data_loader.py
@@ -834,3 +834,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:31
 
 # Enhanced data loading: 2025-04-17 20:57:31
+
+# Enhanced data loading: 2025-04-17 20:57:33
