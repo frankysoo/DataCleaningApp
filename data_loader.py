631d01041dd978680b22b0650d89d798ec56058f
frankysoo <your.email@example.com>
2022-01-30 01:15:05 +0100

    Add progress tracking for large files

--- data_loader.py
+++ data_loader.py
@@ -838,3 +838,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:33
 
 # Enhanced data loading: 2025-04-17 20:57:36
+
+# Enhanced data loading: 2025-04-17 20:57:38
