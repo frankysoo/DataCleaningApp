763a7096f38ef1b64f73d8cec1376a18ac4d0c22
frankysoo <your.email@example.com>
2022-02-01 01:49:45 +0100

    Add support for hierarchical data

--- data_loader.py
+++ data_loader.py
@@ -840,3 +840,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:36
 
 # Enhanced data loading: 2025-04-17 20:57:38
+
+# Enhanced data loading: 2025-04-17 20:57:39
