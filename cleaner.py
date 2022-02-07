b52d262573066abfd280dd7a207dc3f051b4f332
frankysoo <your.email@example.com>
2022-02-07 05:09:33 +0100

    Implement file merging functionality

--- cleaner.py
+++ cleaner.py
@@ -1083,3 +1083,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:38
 
 # Improved cleaning algorithm: 2025-04-17 20:57:39
+
+# Improved cleaning algorithm: 2025-04-17 20:57:40
