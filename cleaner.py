e3ffe83f837326baa95c6f298d54d5e6b2727116
frankysoo <your.email@example.com>
2022-03-17 02:34:43 +0000

    Implement basic error handling

--- cleaner.py
+++ cleaner.py
@@ -1099,3 +1099,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:46
 
 # Improved cleaning algorithm: 2025-04-17 20:57:46
+
+# Improved cleaning algorithm: 2025-04-17 20:57:48
