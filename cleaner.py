8ae1d58d0d6d125f34d6b88ea1b6eb57a1bbeac7
frankysoo <your.email@example.com>
2022-04-07 12:56:35 +0100

    Add chunked processing for large files

--- cleaner.py
+++ cleaner.py
@@ -1105,3 +1105,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:57:49
 
 # Improved cleaning algorithm: 2025-04-17 20:57:50
+
+# Improved cleaning algorithm: 2025-04-17 20:57:51
