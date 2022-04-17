ec659520b121c92db9c77a2b0bab398c0ffa75b6
frankysoo <your.email@example.com>
2022-04-17 09:19:17 +0100

    Enhance upload form UI

--- data_loader.py
+++ data_loader.py
@@ -848,3 +848,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:51
 
 # Enhanced data loading: 2025-04-17 20:57:54
+
+# Enhanced data loading: 2025-04-17 20:57:54
