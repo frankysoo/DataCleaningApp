06e0f94d4cec4e60fdbc5524ba30fd9c0f259d8b
frankysoo <your.email@example.com>
2022-04-23 18:13:32 +0100

    Add column statistics display

--- data_loader.py
+++ data_loader.py
@@ -854,3 +854,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:56
 
 # Enhanced data loading: 2025-04-17 20:57:56
+
+# Enhanced data loading: 2025-04-17 20:57:57
