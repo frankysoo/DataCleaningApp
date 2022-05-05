b233a5fb239a47851f89bf8b641ad0fa2a98c84d
frankysoo <your.email@example.com>
2022-05-05 08:27:30 +0100

    Enhance email validation with domain checking

--- data_loader.py
+++ data_loader.py
@@ -862,3 +862,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:57:58
 
 # Enhanced data loading: 2025-04-17 20:57:59
+
+# Enhanced data loading: 2025-04-17 20:58:00
