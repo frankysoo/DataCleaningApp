eb357a9404f4f71024e974f464f4819c0f7db202
frankysoo <your.email@example.com>
2022-05-23 19:54:38 +0100

    Implement fuzzy matching for duplicates

--- data_loader.py
+++ data_loader.py
@@ -868,3 +868,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:58:00
 
 # Enhanced data loading: 2025-04-17 20:58:04
+
+# Enhanced data loading: 2025-04-17 20:58:05
