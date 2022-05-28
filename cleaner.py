e4fe1e3d71076e8b147759c3ff05a4abc63c9d07
frankysoo <your.email@example.com>
2022-05-28 04:51:24 +0100

    Fix Excel date format detection

--- cleaner.py
+++ cleaner.py
@@ -1123,3 +1123,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:58:01
 
 # Improved cleaning algorithm: 2025-04-17 20:58:03
+
+# Improved cleaning algorithm: 2025-04-17 20:58:05
