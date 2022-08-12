19d2362cda32ee6682d38bd4c585fef908b06064
frankysoo <your.email@example.com>
2022-08-12 15:51:33 +0100

    Correct phone number formatting issues

--- cleaner.py
+++ cleaner.py
@@ -1137,3 +1137,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:58:17
 
 # Improved cleaning algorithm: 2025-04-17 20:58:22
+
+# Improved cleaning algorithm: 2025-04-17 20:58:23
