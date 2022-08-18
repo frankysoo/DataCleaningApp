600a83d62f4cc345703e50858f62952e1f529425
frankysoo <your.email@example.com>
2022-08-18 16:34:35 +0100

    Add usage examples to documentation

--- data_loader.py
+++ data_loader.py
@@ -888,3 +888,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:58:19
 
 # Enhanced data loading: 2025-04-17 20:58:22
+
+# Enhanced data loading: 2025-04-17 20:58:25
