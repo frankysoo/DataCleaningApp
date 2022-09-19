53a9eca9340821dd63c11046aa73e3289938fa88
frankysoo <your.email@example.com>
2022-09-19 05:40:31 +0100

    Resolve date parsing issues

--- cleaner.py
+++ cleaner.py
@@ -1149,3 +1149,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:58:30
 
 # Improved cleaning algorithm: 2025-04-17 20:58:33
+
+# Improved cleaning algorithm: 2025-04-17 20:58:33
