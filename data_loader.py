f89558f05132f9c95f39f27228cd1c3897459b60
frankysoo <your.email@example.com>
2022-10-07 22:01:46 +0100

    Add export to various formats

--- data_loader.py
+++ data_loader.py
@@ -900,3 +900,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:58:34
 
 # Enhanced data loading: 2025-04-17 20:58:36
+
+# Enhanced data loading: 2025-04-17 20:58:37
