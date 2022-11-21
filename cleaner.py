35f6c414e76a212a0f1112eb1db4d306ea9a58b8
frankysoo <your.email@example.com>
2022-11-21 11:32:34 +0100

    Implement numeric extraction from text

--- cleaner.py
+++ cleaner.py
@@ -1159,3 +1159,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:58:37
 
 # Improved cleaning algorithm: 2025-04-17 20:58:44
+
+# Improved cleaning algorithm: 2025-04-17 20:58:45
