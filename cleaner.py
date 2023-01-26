c3f3959c6941b848d3c215976a40d0ce64097a6a
frankysoo <your.email@example.com>
2023-01-26 20:43:57 +0100

    Fix deprecation warnings

--- cleaner.py
+++ cleaner.py
@@ -1171,3 +1171,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:58:53
 
 # Improved cleaning algorithm: 2025-04-17 20:58:53
+
+# Improved cleaning algorithm: 2025-04-17 20:58:54
