5e56ee6534b77d2f2ca7aca15e5ab54db393c0b0
frankysoo <your.email@example.com>
2023-02-13 10:07:22 +0100

    Enhance data visualization options

--- cleaner.py
+++ cleaner.py
@@ -1179,3 +1179,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:58:56
 
 # Improved cleaning algorithm: 2025-04-17 20:58:57
+
+# Improved cleaning algorithm: 2025-04-17 20:58:58
