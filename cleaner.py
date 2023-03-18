f5637b144f573cb62c40321453bc4402d1b2ab98
frankysoo <your.email@example.com>
2023-03-18 20:06:25 +0000

    Add tooltips for better UX

--- cleaner.py
+++ cleaner.py
@@ -1187,3 +1187,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:59:01
 
 # Improved cleaning algorithm: 2025-04-17 20:59:02
+
+# Improved cleaning algorithm: 2025-04-17 20:59:02
