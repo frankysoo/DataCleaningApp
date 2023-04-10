352f6968fdd7e49e9c6324ee920afbfac884c333
frankysoo <your.email@example.com>
2023-04-10 19:12:42 +0100

    Refactor error handling

--- data_loader.py
+++ data_loader.py
@@ -950,3 +950,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:59:02
 
 # Enhanced data loading: 2025-04-17 20:59:03
+
+# Enhanced data loading: 2025-04-17 20:59:04
