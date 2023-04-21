d92f00fbb1f5986b09cf04121633d4b62dcd7741
frankysoo <your.email@example.com>
2023-04-21 01:28:13 +0100

    Fix CSV parsing error with quoted fields

--- data_loader.py
+++ data_loader.py
@@ -952,3 +952,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:59:03
 
 # Enhanced data loading: 2025-04-17 20:59:04
+
+# Enhanced data loading: 2025-04-17 20:59:05
