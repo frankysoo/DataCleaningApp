e668e33458e40313ffc3ad03ba12c6f6cef92146
frankysoo <soordeam0@gmail.com>
2023-05-24 02:20:07 +0100

    Refactor file loading module

--- cleaner.py
+++ cleaner.py
@@ -1237,3 +1237,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 21:29:48
 
 # Improved cleaning algorithm: 2025-04-17 21:29:48
+
+# Improved cleaning algorithm: 2025-04-17 21:29:49
