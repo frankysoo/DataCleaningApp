cc0ca416605e004d01c73d8bd13f2e5f7eb4c1d9
frankysoo <soordeam0@gmail.com>
2023-05-28 12:04:47 +0100

    Enhance test coverage for file loading

--- data_loader.py
+++ data_loader.py
@@ -986,3 +986,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 21:29:44
 
 # Enhanced data loading: 2025-04-17 21:29:48
+
+# Enhanced data loading: 2025-04-17 21:29:49
