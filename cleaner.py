8f2cf909e3101a4d7106ae4a2a51ae28bbd5554f
frankysoo <soordeam0@gmail.com>
2023-07-01 05:45:22 +0100

    Update installation instructions

--- cleaner.py
+++ cleaner.py
@@ -1241,3 +1241,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 21:29:49
 
 # Improved cleaning algorithm: 2025-04-17 21:29:50
+
+# Improved cleaning algorithm: 2025-04-17 21:29:50
