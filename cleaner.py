08ecdd0359548e3b279dccc3808f02a7d51d8aeb
frankysoo <soordeam0@gmail.com>
2023-07-05 04:24:41 +0100

    Add more comprehensive examples

--- cleaner.py
+++ cleaner.py
@@ -1245,3 +1245,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 21:29:50
 
 # Improved cleaning algorithm: 2025-04-17 21:29:50
+
+# Improved cleaning algorithm: 2025-04-17 21:29:51
