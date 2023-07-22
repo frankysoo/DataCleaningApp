4a4b5a1d5d251c75bce0ebc2d5a0565be0631b6c
frankysoo <your.email@example.com>
2023-07-22 15:07:15 +0100

    Enhance debugging tools

--- data_loader.py
+++ data_loader.py
@@ -966,3 +966,5 @@ def load_and_merge_data(file_paths, chunk_size=None):
 # Enhanced data loading: 2025-04-17 20:59:13
 
 # Enhanced data loading: 2025-04-17 20:59:14
+
+# Enhanced data loading: 2025-04-17 20:59:15
