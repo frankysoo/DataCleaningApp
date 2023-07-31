89121e4ce6addd66295a16ade6ca478b4ca218c2
frankysoo <your.email@example.com>
2023-08-01 00:00:00 +0100

    Add Bootstrap for styling

--- cleaner.py
+++ cleaner.py
@@ -1201,3 +1201,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:59:11
 
 # Improved cleaning algorithm: 2025-04-17 20:59:15
+
+# Improved cleaning algorithm: 2025-04-17 20:59:17
