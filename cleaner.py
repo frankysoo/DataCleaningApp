4b60448858b4a52e87683f536d06f5faccdabe1d
frankysoo <your.email@example.com>
2023-08-14 00:00:00 +0100

    Optimize imports and dependencies

--- cleaner.py
+++ cleaner.py
@@ -1213,3 +1213,5 @@ def clean_data(df, options=None):
 # Improved cleaning algorithm: 2025-04-17 20:59:21
 
 # Improved cleaning algorithm: 2025-04-17 20:59:23
+
+# Improved cleaning algorithm: 2025-04-17 20:59:23
